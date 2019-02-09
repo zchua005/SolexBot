c0ce12fae4aff1273354a175a63f1d9b25732563
Sheng Yunzhou <shengyunzhou@gmail.com>
2019-02-09 11:24:20 +0800

    Add order open command

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const Telegraf = require("telegraf");
 
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || "https://heroku-app-name.herokuapp.com";
-const API_TOKEN = "TOKEN_FROM_BOT_FATHER";
+const API_TOKEN = "630082018:AAEPN7VB-Lhg8qY_ye5BXEdecen0UASYt6g";
 const bot = new Telegraf(API_TOKEN);
 
 bot.use(Telegraf.log());
 
+bot.help(({ reply }) => reply("Hello I'm the Solex Bot"));
+
+const coffeeOrderSessions = [];
+
+bot.command("orderopen", ctx => {
+  const { from } = ctx.update.message;
+  const coffeeGif = "CgADBAADdwADg731Uoi1wlQtUhaEAg";
+  ctx.replyWithAnimation(coffeeGif, { caption: "Coffee Order Open!" });
+  const newSession = {
+    from,
+    orders: [],
+    isOrderOpen: true
+  };
+  coffeeOrderSessions.push(newSession);
+});
+
 bot.start(ctx => ctx.reply("Hello"));
 
 if (process.env.NODE_ENV === "production") {
-  console.log("Production!");
   bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
   bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 } else {
